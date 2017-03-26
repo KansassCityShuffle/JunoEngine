1ceb2a381b7cb953325c624bc0fb6fce9f10754c
KansassCityShuffle <lisa.aubry@insa-cvl.fr>
2017-03-26 13:44:19 +0200

    add robot frame and position manager

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,7 @@
 #include "detectionwidget.h"
 #include "slamwidget.h"
 #include <QSplitter>
-
+#include <QKeyEvent>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -18,18 +18,18 @@ MainWindow::MainWindow(QWidget *parent) :
     format.setProfile(QSurfaceFormat::CoreProfile);
     format.setSamples(8);
     format.setVersion(4, 5);
-
+/*
     DetectionWidget* dw = new DetectionWidget();
     dw->setFormat(format);
     dw->resize(QSize(256, 128));
-
+*/
     SlamWidget* sw = new SlamWidget();
     sw->setFormat(format);
     sw->resize(QSize(256, 128));
-
+    sw->setFocusPolicy(Qt::ClickFocus);
     QSplitter *splitter = new QSplitter();
     splitter->setOrientation(Qt::Horizontal);
-    splitter->addWidget(dw);
+//    splitter->addWidget(dw);
     splitter->addWidget(sw);
 
     ui->verticalLayout->addWidget(splitter);
@@ -40,3 +40,4 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
